3d64714e2b65c9792cd3f63783b22378a5b64bf4
Sol Nam <cbkpwr@gmail.com>
2024-04-02 00:19:06 -0500

    6.4 image using a HTML image tag

--- index.php
+++ index.php
@@ -3,37 +3,62 @@
 
 <?php
 
-    $posts = $conn->query("SELECT * FROM posts");
-    $posts->execute();
-    $rows = $posts->fetchAll(PDO::FETCH_OBJ);
+$posts = $conn->query("SELECT * FROM posts");
+$posts->execute();
+$rows = $posts->fetchAll(PDO::FETCH_OBJ);
+
+
+$categories = $conn->query("SELECT * FROM categories");
+$categories->execute();
+$category = $categories->fetchAll(PDO::FETCH_OBJ);
 
 ?>
 
 
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-md-10 col-lg-8 col-xl-7">
+<div class="row gx-4 gx-lg-5 justify-content-center">
+    <div class="col-md-10 col-lg-8 col-xl-7">
 
 
-                <?php foreach($rows as $row) : ?>
+        <?php foreach ($rows as $row) : ?>
 
-                    <!-- Post preview-->
-                    <div class="post-preview">
-                        <a href="http://localhost:31337/project/posts/post.php?post_id=<?php echo $row->id; ?>">
-                            <h2 class="post-title"><?php echo $row->title; ?></h2>
-                            <h3 class="post-subtitle"><?php echo $row->subtitle; ?></h3>
-                        </a>
-                        <p class="post-meta">
-                            Posted by
-                            <a href="#!"><?php echo $row->user_name; ?></a>
-                            <?php echo date('M', strtotime($row->created_at)) . ' ' . date('d', strtotime($row->created_at)) . ', ' . date('Y', strtotime($row->created_at)); ?>
-                        </p>
-                    </div>
-                    <!-- Divider-->
-                    <hr class="my-4" />
-                    <?php endforeach; ?>
-                    <!-- Pager-->
-                    
-                </div>
+            <!-- Post preview-->
+            <div class="post-preview">
+                <a href="http://localhost:31337/project/posts/post.php?post_id=<?php echo $row->id; ?>">
+                    <h2 class="post-title"><?php echo $row->title; ?></h2>
+                    <h3 class="post-subtitle"><?php echo $row->subtitle; ?></h3>
+                </a>
+
+                <?php if (!empty($row->img)) : ?>
+                    <img src="http://localhost:31337/project/images/<?php echo htmlspecialchars($row->img); ?>" alt="<?php echo htmlspecialchars($row->title); ?>" style="width:100%;max-width:600px;">
+                <?php endif; ?>
+
+                <p class="post-meta">
+                    Posted by
+                    <a href="#!"><?php echo $row->user_name; ?></a>
+                    <?php echo date('M', strtotime($row->created_at)) . ' ' . date('d', strtotime($row->created_at)) . ', ' . date('Y', strtotime($row->created_at)); ?>
+                </p>
             </div>
+            <!-- Divider-->
+            <hr class="my-4" />
+        <?php endforeach; ?>
+        <!-- Pager-->
+
+    </div>
+</div>
+<div class="row gx-4 gx-lg-5 justify-content-center">
+            <H3>Categories</H3>
+            <br>
+            <br>
+            <br>
+            
+    <?php foreach ($category as $cat) : ?>
+        <div class="col-md-6">
+        <a href="http://localhost:31337/project/categories/category.php?cat_id=<?php echo $cat->id; ?>">
+            <div class="alert alert-dark bg-dark text-center text-white" role="alert">
+                <?php echo $cat->name; ?>
+            </div></a>
+        </div>
+    <?php endforeach; ?>
+</div>
 
 <?php require "includes/footer.php"; ?>
