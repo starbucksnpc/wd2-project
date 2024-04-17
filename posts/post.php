6d3e53f40f51831b5d1ef96802b31e57c92c8d40
Sol Nam <cbkpwr@gmail.com>
2024-04-16 23:06:06 -0500

    Creating the comments
    This is the first part of comments.

--- posts/post.php
+++ posts/post.php
@@ -27,6 +27,25 @@ if (isset($_GET['post_id'])) {
         exit;
     }
 }
+
+
+if(isset($_POST['submit']) AND isset($_GET['post_id'])) {
+    //the id of the post and the username for the who posted the comment
+    $id = $_GET['post_id'];
+    $user_name = $_SESSION['username'];
+    $comment = $_POST['comment'];
+
+    $insert = $conn->prepare("INSERT INTO comments (id_post_comment, user_name_comment, comment) VALUES (:id_post_comment, :user_name_comment, :comment)");
+
+    $insert->execute([
+        ':id_post_comment' => $id,
+        ':user_name_comment' => $user_name,
+        ':comment' => $comment,
+
+    ]);
+
+    header("location: http://localhost:31337/project/posts/post.php?post_id=".$id."");
+}
 ?>
 
 <!-- Page Header-->
@@ -72,5 +91,71 @@ if (isset($_GET['post_id'])) {
         </div>
     </div>
 </article>
+
+
+
+<section>
+          <div class="container my-5 py-5">
+            <div class="row d-flex justify-content-center">
+              <div class="col-md-12 col-lg-10 col-xl-8">
+                <h3 class="mb-5">Comments</h3>
+
+                <div class="card">
+                  <div class="card-body">
+                    <div class="d-flex flex-start align-items-center">
+                    
+                        <div>
+                            <h6 class="fw-bold text-primary">Lily Coleman<h8 class="p-3 text-black">(Jun 2020)</h8></h6>
+                            
+                        </div>
+                        </div>
+
+                        <p class="mt-3 mb-4 pb-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.
+                        </p>
+                   
+
+                        <hr class="my-4" />
+
+                        <div class="d-flex flex-start align-items-center">
+                        <div>
+                            <h6 class="fw-bold text-primary">Lily Coleman<h8 class="p-3 text-black">(Jun 2020)</h8></h6>
+                            
+                        </div>
+                        </div>
+
+                        <p class="mt-3 mb-4 pb-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip consequat.
+                        </p>
+
+                 
+                  </div>
+                  <form method="POST" action="post.php?post_id=<?php echo $id; ?>">
+
+                        <div class="card-footer py-3 border-0" style="background-color: #f8f9fa;">
+
+                            <div class="d-flex flex-start w-100">
+                            
+                                <div class="form-outline w-100">
+                                    <textarea class="form-control" id="" placeholder="write message" rows="4"
+                                     name="comment"></textarea>
+                                
+                                </div>
+                            </div>
+                            <div class="float-end mt-2 pt-1">
+                                <button type="submit" name="submit" class="btn btn-primary btn-sm mb-3">Post comment</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
 
 <?php require "../includes/footer.php"; ?>
